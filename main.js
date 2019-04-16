ba742883598c597229e4fde7d54b470d3802d478
nanofuxion <nanofuxion@users.noreply.github.com>
2019-04-16 19:00:46 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function timeConverter(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+        minutes = "00";
+    } else if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+}
---
+++ questions.js
@@ -0,0 +1,6 @@
+var questions = [
+    { question: "When did the first episode of The Cosby Show air?", answer: ["September 20, 1984", "corret"], answer: ["July 15, 1984", "wrong"], answer: ["August 05, 1984", "wrong"], answer: ["November 01, 1984", "wrong"] },
+    { question: "When did the first episode of fresh prince air?", answer: ["September 15, 1995", "wrong"], answer: ["September 10, 1990", "correct"], answer: ["September 10, 1999", "wrong"], answer: ["September 01, 2001", "wrong"] },
+    { question: "Who played Denise Huxtable in The Cosby Show?", answer: [" Zoë Kravitz", "wrong"], answer: ["Lenny Kravitz ", "wrong"], answer: [" Arlene Litman", "wrong"], , answer: ["Lisa Bonet", "correct"], },
+    { question: , answer: , answer: , answer: , answer: },
+]
