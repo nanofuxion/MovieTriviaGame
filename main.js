8a519ed51ddcdf76db767aab6664df75c873814a
nanofuxion <nanofuxion@users.noreply.github.com>
2019-05-10 19:54:27 -0400

    updated js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+    $(".mdl-button").on("click", btnPressed);
+};
+isStarted = false;
+questionNumb = {};
+questionIncrement = 1;
+
+
 function timeConverter(t) {
 
     var minutes = Math.floor(t / 60);
@@ -12,6 +20,26 @@ function timeConverter(t) {
     } else if (minutes < 10) {
         minutes = "0" + minutes;
     }
-
     return minutes + ":" + seconds;
+}
+
+function btnPressed() {
+    if (!isStarted) {
+        isStarted = true;
+        if ($(this).hasClass("mdl-js-ripple-effect")) alert("The button wasn't special sorry 😜");
+        console.log($(this));
+        //questionNumb.oof = "oof0";
+        // console.log(questionNumb.oof);
+    }
+
+}
+
+function getQnA() {
+    questionNumb
+
+}
+
+
+function resetGame() {
+    var bodyDefault = $(body).html();
 }
--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 var questions = [
-    { question: "When did the first episode of The Cosby Show air?", answer: ["September 20, 1984", "corret"], answer: ["July 15, 1984", "wrong"], answer: ["August 05, 1984", "wrong"], answer: ["November 01, 1984", "wrong"] },
-    { question: "When did the first episode of fresh prince air?", answer: ["September 15, 1995", "wrong"], answer: ["September 10, 1990", "correct"], answer: ["September 10, 1999", "wrong"], answer: ["September 01, 2001", "wrong"] },
-    { question: "Who played Denise Huxtable in The Cosby Show?", answer: [" Zoë Kravitz", "wrong"], answer: ["Lenny Kravitz ", "wrong"], answer: [" Arlene Litman", "wrong"], , answer: ["Lisa Bonet", "correct"], },
-    { question: , answer: , answer: , answer: , answer: },
+    { question: "When did the first episode of The Cosby Show air?", answer1: ["September 20, 1984", "corret"], answer2: ["July 15, 1984", "wrong"], answer3: ["August 05, 1984", "wrong"], answer4: ["November 01, 1984", "wrong"] },
+    { question: "When did the first episode of fresh prince air?", answer1: ["September 15, 1995", "wrong"], answer2: ["September 10, 1990", "correct"], answer3: ["September 10, 1999", "wrong"], answer4: ["September 01, 2001", "wrong"] },
+    { question: "Who played Denise Huxtable in The Cosby Show?", answer1: [" Zoë Kravitz", "wrong"], answer2: ["Lenny Kravitz ", "wrong"], answer3: [" Arlene Litman", "wrong"], answer4: ["Lisa Bonet", "correct"], },
+    //{ question: , answer: , answer: , answer: , answer: } { question: , answer: , answer: , answer: , answer: }
 ]
