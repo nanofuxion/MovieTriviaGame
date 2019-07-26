fe6271e25a68cb25eeb9e787d5121d317bf72797
nanofuxion <nanofuxion@users.noreply.github.com>
2019-07-26 18:07:50 -0400

    updated JS, obscured answers in questions.js and refactored code to allow modificaton of questions.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-window.onload = function() {
+window.onload = function () {
     $(".mdl-button").on("click", btnPressed);
 };
 isStarted = false;
-questionNumb = {};
-questionIncrement = 1;
+var timePerQ = 30;
+var questionNumb;
+var QCheck = {};
+var questionIncrement = 0;
+var intervalId;
 
 
 function timeConverter(t) {
@@ -30,16 +33,106 @@ function btnPressed() {
         console.log($(this));
         //questionNumb.oof = "oof0";
         // console.log(questionNumb.oof);
+        nextQuestion(questionIncrement);
+    } else {
+        getQnA(parseInt($(this).attr('id').slice(-1)) - 1, questionIncrement-1, $("#question").html(), $(this).html());
     }
 
 }
 
-function getQnA() {
-    questionNumb
+function getQnA(ans, val, Q, A) {
+    var questionIncrement = val;
+    console.log("the answer var" + ans);
+    console.log("the correct answer var" + (parseInt(cAnswers[questionIncrement], 2) - 10));
+    if (ans == (parseInt(cAnswers[questionIncrement], 2) - 10)) {
+        console.log("correct");
+        console.log("answer"+(questionIncrement + 1));
+        QCheck[("q"+questionIncrement)]=[Q, A,"Correct"];
+    } 
+    else{
+        QCheck[("q"+questionIncrement)]=[Q, A,"Wrong"];
+    }
+    nextQuestion(questionIncrement + 1);
+}
+
+function nextQuestion(questionNumb) {
+
+    if (questions[questionNumb] != undefined) {
+        startTimer();
+        $("#question").html(questions[questionNumb].question);
+        $("#bt1").html(questions[questionNumb].answer1);
+        $("#bt2").html(questions[questionNumb].answer2);
+        $("#bt3").html(questions[questionNumb].answer3);
+        $("#bt4").html(questions[questionNumb].answer4);
+        questionIncrement = questionNumb + 1;
+    } else {
+        clearInterval(intervalId);
+        showScore();
+    }
+
 
 }
 
 
-function resetGame() {
-    var bodyDefault = $(body).html();
+function startTimer() {
+    clearInterval(intervalId);
+    timePerQ = 30;
+    intervalId = setInterval(function () {
+        if (timePerQ > -1) {
+            $("#display").html(timeConverter(timePerQ));
+            timePerQ = timePerQ - 1;
+        }
+        else {
+            QCheck[("q"+(questionIncrement-1))]=[$("#question").html(), "","Unanswered"];
+            nextQuestion(questionIncrement);
+            timePerQ = 30;
+        }
+
+    },
+        500);
+
 }
+
+function showScore() {
+    $("#outPopUp").css({
+        "top": "15%",
+        "left": "10%",
+        "bottom": "5%",
+        "right": "10%",
+        "background-color": "#e9ecef",
+        "border-width":  "4px",
+        "border-color": "black"
+    });
+    var results = $("<div class='span mx-auto'>");
+    var h1 = $("<h1>").html("Done!").appendTo(results);
+    var tallyCorrct = 0;
+    var tallyWrong = 0;
+    var tallyUnanswered = 0;
+    for (let i = 0; i < Object.keys(QCheck).length; i++) {
+
+        
+        // var span = $("<div class='span'>);
+        $("<p>").html(QCheck["q"+i][0]).appendTo(results);
+        $("<p>").html(QCheck["q"+i][2]).appendTo(results);    
+        if(QCheck["q"+i][2] == "Correct") tallyCorrct = tallyCorrct + 1;
+        if(QCheck["q"+i][2] == "Wrong") tallyWrong = tallyWrong + 1;
+        if(QCheck["q"+i][2] == "Unanswered") tallyUnanswered = tallyUnanswered + 1;
+        if (i == Object.keys(QCheck).length-1)  $("<p>").html("<hr>" + "Totall correct: " + tallyCorrct + "<br>" + "Totall Wrong: " + tallyWrong +  "<br>" + "Totall Unanswered: " + tallyUnanswered).appendTo(results);
+        results.appendTo("#sonOFoutPopUp");  
+    }
+
+
+    // QCheck.forEach(element => {
+    //    $("<p>").html(questions[questionNumb].question) .appendTo("#sonOFoutPopUp");
+    // });
+
+
+
+}
+
+
+
+//not an assignment requirement
+// function resetGame() {
+//     var bodyDefault = $(body).html();
+// }
--- questions.js
+++ questions.js
@@ -1,6 +1,25 @@
+
 var questions = [
-    { question: "When did the first episode of The Cosby Show air?", answer1: ["September 20, 1984", "corret"], answer2: ["July 15, 1984", "wrong"], answer3: ["August 05, 1984", "wrong"], answer4: ["November 01, 1984", "wrong"] },
-    { question: "When did the first episode of fresh prince air?", answer1: ["September 15, 1995", "wrong"], answer2: ["September 10, 1990", "correct"], answer3: ["September 10, 1999", "wrong"], answer4: ["September 01, 2001", "wrong"] },
-    { question: "Who played Denise Huxtable in The Cosby Show?", answer1: [" Zoë Kravitz", "wrong"], answer2: ["Lenny Kravitz ", "wrong"], answer3: [" Arlene Litman", "wrong"], answer4: ["Lisa Bonet", "correct"], },
-    //{ question: , answer: , answer: , answer: , answer: } { question: , answer: , answer: , answer: , answer: }
+    { question: "When did the first episode of The Cosby Show air?", answer1: "September 20, 1984", answer2: "July 15, 1984", answer3: "August 05, 1984", answer4: "November 01, 1984" },
+    { question: "When did the first episode of fresh prince air?", answer1: "September 15, 1995", answer2: "September 10, 1990", answer3: "September 10, 1999", answer4: "September 01, 2001" },
+    { question: "Who played Denise Huxtable in The Cosby Show?", answer1: "Zoë Kravitz", answer2: "Lenny Kravitz ", answer3: "Arlene Litman", answer4: "Lisa Bonet", },
+    { question: "When did the TV show Rick and Morty first air on Adult Swim?", answer1: "2013", answer2: "2014", answer3: "2015", answer4: "2016"}, 
+    { question: "In the series Twin Peaks, who killed Laura Palmer?", answer1: "James Hurley", answer2: "Laura Palmer, herself", answer3: "Killer BOB", answer4: "Lucy Moran"},
+
+    //{ question: , answer: , answer: , answer: , answer: }, { question: , answer: , answer: , answer: , answer: }
 ]
+
+//aswers are written in binary to discourage cheating. 😜
+var cAnswers =[
+    "1010",
+    "1011",
+    "1101",
+    "1010",
+    "1100",
+]
+
+
+// 0 1010
+// 1 1011
+// 2 1100
+// 3 1101
